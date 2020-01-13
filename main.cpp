272f289139e29591466f4582290d65aafbb9dfc1
elizabethLivingston666x <59808728+elizabethLivingston666x@users.noreply.github.com>
2020-01-12 18:59:58 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+		int count = 1;
+
+		cout << "ElizabethLivingston666: LivingstonElizabeth" <<endl;
+
+
+		do{
+			cout << count << " ";
+			if (count % 5 == 0){
+				cout << endl;
+			}
+			count++;
+
+		}
+		while (count <= 100);
+return 0;
+}
